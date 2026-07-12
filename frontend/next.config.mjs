070fe061043b2f5303ffc1ec77dfc7b0d5bcf4d2
Dushyant Saini <dushyantsaini@Dushyants-MacBook-Pro.local>
2026-07-12 19:17:18 +0530

    feat: set Cross-Origin-Opener-Policy same-origin-allow-popups header to allow Google Sign-in popup communication

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -31,6 +31,19 @@ const nextConfig = {
       },
     ]
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
